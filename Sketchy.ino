70024e73b5b641c4e47f915102db8f4cd6a7d0da
Adam Haberlach <adam@mediariffic.com>
2015-06-28 15:49:11 -0700

    Send commands over the serial connection.
    xp,xm,yp,ym (for x/y pos/minus) and a count will move that far.

--- Sketchy.ino
+++ Sketchy.ino
@@ -9,9 +9,13 @@ int  X_STEP_PIN = 12;
  
 int Y_DIR_PIN = 11;
 int Y_STEP_PIN = 10;
- 
+
+char inbuffer[200];
+
 // the setup routine runs once when you press reset:
-void setup() {                
+void setup() {
+  Serial.begin(19200);
+  
   pinMode(X_DIR_PIN, OUTPUT); // HIGH == to the right
   pinMode(X_STEP_PIN, OUTPUT);
   pinMode(Y_DIR_PIN, OUTPUT);
@@ -39,25 +43,70 @@ void step_y(boolean dir) {
   step_motor(dir, Y_DIR_PIN, Y_STEP_PIN); 
 }
 
-void plot_square(int size) {
-  for (int x = 0; x < size; x++) {
-    step_x(true);
-  }
-  for (int x = 0; x < size; x++) {
-    step_y(true);
-  }
-  for (int x = 0; x < size; x++) {
+void move_left(int count) {
+  for (int i = 0; i < count; i++) {
     step_x(false);
   }
-  for (int x = 0; x < size; x++) {
+}
+
+void move_right(int count) {
+  for (int i = 0; i < count; i++) {
+    step_x(true);
+  }
+}
+
+void move_up(int count) {
+  for (int i = 0; i < count; i++) {
+    step_y(true);
+  }
+}
+
+void move_down(int count) {
+  for (int i = 0; i < count; i++) {
     step_y(false);
   }
 }
 
+void plot_square(int size) {
+  move_up(size);
+  move_right(size);
+  move_down(size);
+  move_left(size);
+}
+
 void loop() {
-  plot_square(2000);
-
-
-
-  while(true);
+  if (Serial.available()) {
+    byte size = Serial.readBytes(inbuffer, 200);
+    // Note: this assume we got a carriage-return terminated string
+    inbuffer[size-1] = 0;
+    Serial.print("Buffer: ");
+    Serial.println(inbuffer);
+    char* command = strtok(inbuffer, " ");
+    while (command != 0) {
+      if (0 == strcmp(command, "xm")) {
+        int count = atoi(strtok(0, " "));
+        Serial.println("Going left");
+        move_left(count);
+      } else if (0 == strcmp(command, "xp")) {
+        int count = atoi(strtok(0, " "));
+        Serial.println("Going right");
+        move_right(count);
+      } else if (0 == strcmp(command, "yp")) {
+        int count = atoi(strtok(0, " "));
+        Serial.println("Going up");
+        move_up(count);
+      } else if (0 == strcmp(command, "ym")) {
+        int count = atoi(strtok(0, " "));
+        Serial.println("Going down");
+        move_down(count);
+      } else {
+        Serial.print("Unknown command: \"");
+        Serial.print(command);
+        Serial.println("\"");
+      }
+      
+      
+      command = strtok(0, " ");
+    }
+  }
 }
